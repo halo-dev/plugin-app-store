9b7a15daaa7bbc5a6b0005bfe573aba9b047c609
Ryan Wang <i@ryanc.cc>
2023-12-02 12:06:08 +0800

    chore: use @halo-dev/ui-plugin-bundler-kit to refactor vite config (#47)
    使用 Halo 新提供的 `@halo-dev/ui-plugin-bundler-kit` 包重构 Vite 打包配置。
    
    see https://github.com/halo-dev/halo/pull/4916
    
    ```release-note
    None
    ```

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -1,62 +1,21 @@
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Icons from "unplugin-icons/vite";
 import Markdown from "unplugin-vue-markdown/vite";
+import { HaloUIPluginBundlerKit } from "@halo-dev/ui-plugin-bundler-kit";
 
-const pluginEntryName = "app-store-integration";
-
-export default ({ mode }: { mode: string }) => {
-  const isProduction = mode === "production";
-  const outDir = isProduction ? "../src/main/resources/console" : "../build/resources/main/console";
-
-  return defineConfig({
-    plugins: [
-      Vue({ include: [/\.vue$/, /\.md$/], script: { defineModel: true } }),
-      Icons({ compiler: "vue3" }),
-      Markdown(),
-    ],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
+export default defineConfig({
+  plugins: [
+    Vue({ include: [/\.vue$/, /\.md$/], script: { defineModel: true } }),
+    Icons({ compiler: "vue3" }),
+    Markdown({}),
+    HaloUIPluginBundlerKit() as Plugin,
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    define: {
-      "process.env": process.env,
-    },
-    build: {
-      outDir,
-      emptyOutDir: true,
-      lib: {
-        entry: "src/index.ts",
-        name: pluginEntryName,
-        formats: ["iife"],
-        fileName: () => "main.js",
-      },
-      rollupOptions: {
-        external: [
-          "vue",
-          "vue-router",
-          "@vueuse/core",
-          "@vueuse/components",
-          "@vueuse/router",
-          "@halo-dev/shared",
-          "@halo-dev/components",
-        ],
-        output: {
-          globals: {
-            vue: "Vue",
-            "vue-router": "VueRouter",
-            "@vueuse/core": "VueUse",
-            "@vueuse/components": "VueUse",
-            "@vueuse/router": "VueUse",
-            "@halo-dev/console-shared": "HaloConsoleShared",
-            "@halo-dev/components": "HaloComponents",
-          },
-          extend: true,
-        },
-      },
-    },
-  });
-};
+  },
+});
